c17f505e278ca7f92ee9d554fbf2f4d1a1949f3d
Noam Perry <noam.perry@hotmail.com>
2021-11-30 17:40:09 +0100

    streamlit widgets

--- bitcoin_prediction.py
+++ bitcoin_prediction.py
@@ -1,3 +1,4 @@
+from enum import auto
 import streamlit as st
 import datetime
 import pandas as pd
@@ -5,17 +6,29 @@ import numpy as np
 import json
 import websocket
 import binance
+from PIL import Image
 
-st.markdown("""# Cryptocurrencies Prediction App""")
+
+st.title("Cryptocurrencies Prediction App")
+
+with st.expander("Useful links"):
+    url1 = 'https://www.coindesk.com/'
+    st.markdown("[CoinDesk: Crypto news and related affairs](%s)" % url1)
+    url2 = 'https://www.binance.com/en'
+    st.markdown("[Binance: Crypto Exchange plateform](%s)" % url2)
+    url3 = 'https://coinmarketcap.com/'
+    st.markdown("[CoinMarketCap: Crypto data agreggator](%s)" % url3)
+
 
 client = binance.Client()
 
-cc = st.selectbox('What crypto do you want to trade?', ['BTCUSDT', 'ETHUSDT', 'BNBUSDT'])
+cc = st.selectbox('What crypto do you want to trade?', ['BTCUSDT', 'ETHUSDT', 'BNBUSDT'], help= 'BTC, ETH and BNB paired with USD tether (stablecoin pegged to $USD)')
 
 cc2 = cc.lower()
 
 interval = st.selectbox('What interval do you want to trade in?',
-                        ['1m', '1w', '1d', '1h', '5m', '1M'])
+                        ['1m', '1w', '1d', '1h', '5m', '1M'],
+                        help='m=minute, h=hour, d=day, w=week, M=month')
 
 socket = f'wss://stream.binance.com:9443/ws/{cc2}@kline_{interval}'
 
@@ -26,11 +39,17 @@ end_date = st.date_input("End Date?", datetime.date(2021, 11, 27))
 end_str = str(end_date.day) + ' ' + str(datetime.date(1900, end_date.month, 1).strftime('%b')) + ', ' + str(end_date.year)
 
 result = client.get_historical_klines(cc, interval, start_str=start_str, end_str=end_str)
+if st.button('Make me a prediction cryptobro'):
+    
+    st.write('here is you prediction babe')
+
 
 df = pd.DataFrame(result)
 
 df[0] = pd.to_datetime(df[0], unit='ms')
 df[6] = pd.to_datetime(df[6], unit='ms')
+
+
 
 df.rename(columns={0: 'Open Time',
                    1: 'Open',
@@ -73,6 +92,19 @@ def on_message(ws, message):
         df.loc[len(df) - 1, 'Close Time'] = pd.to_datetime(df.loc[len(df) - 1, 'Close Time'], unit='ms')
         st.write(f"""Closes: {cs['c']}""")
         st.write(df)
+
+
+st.sidebar.header('Available Tokens 💰 :')
+image = Image.open('/Users/noam/code/nonoperry/data-challenges/BTClogo.png')
+st.sidebar.image(image, width=75, caption='Bitcoin')
+image = Image.open('/Users/noam/code/nonoperry/data-challenges/ETHlogo.png')
+st.sidebar.image(image, width=75, caption='Ethereum')
+image = Image.open('/Users/noam/code/nonoperry/data-challenges/BNBlogo.png')
+st.sidebar.image(image, width=75, caption='Binance Coin')
+st.sidebar.header('Sources 📖 :')
+url = 'https://www.binance.com/fr/support/faq/360002502072'
+st.sidebar.markdown("check out this [link](%s)" % url)
+
 
 ws = websocket.WebSocketApp(socket, on_message=on_message, on_close=on_close)
 
